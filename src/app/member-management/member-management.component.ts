470977d6ff5ecb7bffd69e08d1f9b83ca9f8f084
Ernest-Gray <egray4cs@gmail.com>
2022-02-22 15:45:27 -0500

    basic buttons VERY ROUGH

--- src/app/member-management/member-management.component.ts
+++ src/app/member-management/member-management.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemberManagementComponent implements OnInit {
 
+  //this will have to be downloaded from the servers?
+  moks = ["Mok1", "mok2", "mok3", "mok4", "mok5", "mok6", "mok7", "mok8", "mok9", "mok10", "mok11", "mok12", "mok13", "mok14", "mok15", "mok16"]
+
+  //also will be downloaded from the DB
+  sbangs = ["s1", "s2", "s3"]
+
+  db = "url"
+
+
+  currentMok = 0;
+  changeMok(index:any): void{
+    this.currentMok = index;
+    console.log(index);
+  }
   constructor() { }
 
   ngOnInit() {
