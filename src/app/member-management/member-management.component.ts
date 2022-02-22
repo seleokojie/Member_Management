721ff3e3f4cc75f378b006f69bd687e0aa97b12b
Ernest-Gray <egray4cs@gmail.com>
2022-02-22 17:27:19 -0500

    demonstration of some stuff

--- src/app/member-management/member-management.component.ts
+++ src/app/member-management/member-management.component.ts
@@ -3,27 +3,47 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-member-management',
   templateUrl: './member-management.component.html',
-  styleUrls: ['./member-management.component.css']
+  styleUrls: ['./member-management.component.css'],
 })
 export class MemberManagementComponent implements OnInit {
-
   //this will have to be downloaded from the servers?
-  moks = ["Mok1", "mok2", "mok3", "mok4", "mok5", "mok6", "mok7", "mok8", "mok9", "mok10", "mok11", "mok12", "mok13", "mok14", "mok15", "mok16"]
+  moks = [
+    'Mok1',
+    'mok2',
+    'mok3',
+    'mok4',
+    'mok5',
+    'mok6',
+    'mok7',
+    'mok8',
+    'mok9',
+    'mok10',
+    'mok11',
+    'mok12',
+    'mok13',
+    'mok14',
+    'mok15',
+    'mok16',
+  ];
 
   //also will be downloaded from the DB
-  sbangs = ["s1", "s2", "s3"]
+  //sbangs = { sbangName: 'sbang name', sbangLader: 'id', families: ['famId1'] };
 
-  db = "url"
+  sbangs = ["sbang1", "sbang2", "sbang3", "sbang4", "sbang5"];
+  families = ["fam1" , "fam2", "fam3"];
 
+  db = 'url';
 
+  mokButtonActive = false;
+  toggleMokButton() {
+    this.mokButtonActive = !this.mokButtonActive;
+  }
   currentMok = 0;
-  changeMok(index:any): void{
+  changeMok(index: any): void {
     this.currentMok = index;
     console.log(index);
   }
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
