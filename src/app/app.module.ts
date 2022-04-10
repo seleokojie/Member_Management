ba1d980a98fb43ac40ccc1ebb4ae1c0f69380508
Ernest-Gray <egray4cs@gmail.com>
2022-04-10 14:28:29 -0400

    Added some angular materials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,8 +12,20 @@ import { FamilyComponent } from './family/family.component';
 import { UserEditorComponent } from './user-editor/user-editor.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
+// Add Angular Material Things here?
+import { MatButtonModule } from '@angular/material/button';
+
+//For the auto fill search bar text
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from "@angular/material/input"
+import {MatSelect, MatSelectModule} from '@angular/material/select'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatCardModule} from '@angular/material/card';
 @NgModule({
-  declarations: [			
+  declarations: [
     AppComponent,
       MemberManagementComponent,
       MokjangSelectComponent,
@@ -26,7 +38,20 @@ import { NavigationBarComponent } from './navigation-bar/navigation-bar.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule//for double binding
+    FormsModule,//for double binding,
+
+    ReactiveFormsModule,
+
+    //Add angular Material Modules here as well
+    MatButtonModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatBadgeModule,
+    MatCardModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,5 +1,6 @@
 import { Sarangbang } from './../sarangbang';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -9,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 export class NavigationBarComponent implements OnInit {
   //Holds currently selected drop down menu item
   mokjangSelect = 0;
+  selectedMok = 0;
+  selectedSarang = 0;
   sarangbangSelect = 0;
-  mokjangList = ["Mokjang 1","Mokjang 2","Mokjang 3","Mokjang 4","Mokjang 5","Mokjang 6"]
-  sarangbangList = ["Sarangbang 1","Sarangbang 2","Sarangbang 3","Sarangbang 4","Sarangbang 5","Sarangbang 6"]
+
+  mokjangList = ["Mokjang 1","Mokjang 2","Mokjang 3","Mokjang 4","Mokjang 5","Mokjang 6"];
+  sarangbangList = ["Sarangbang 1","Sarangbang 2","Sarangbang 3","Sarangbang 4","Sarangbang 5","Sarangbang 6"];
+
+  //AUTO FILL SEARCH BAR STUFF
+  //These are the names that are put into the autofill search bar
+  userNames = ["name 1", "name 2", "name 3"];
+  //This is for the autocomplete search bar function
+  myControl = new FormControl();
+
+
 
   //functions
   changeSelectedMokjang(input: any) {
@@ -21,6 +33,10 @@ export class NavigationBarComponent implements OnInit {
   changeSelectedSarangbang(input: any) {
     this.sarangbangSelect = input.target.value;
     console.log("Filtering by sarangbang: " + input.target.value);
+  }
+
+  onSearch(){
+
   }
 
   constructor() {}
